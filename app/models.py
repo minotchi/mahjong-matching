145e720ce4f0b6b82bf914a932aaf1c2554b656d
SHINTARO MINOWA <>
2020-01-12 21:22:24 +0900

    delete sample_1

--- app/models.py
+++ app/models.py
@@ -13,12 +13,12 @@ class Item(models.Model):
     """
 
     # サンプル項目1 文字列
-    sample_1 = models.CharField(
-        verbose_name='サンプル項目1 文字列',
-        max_length=20,
-        blank=True,
-        null=True,
-    )
+    # sample_1 = models.CharField(
+    #     verbose_name='サンプル項目1 文字列',
+    #     max_length=20,
+    #     blank=True,
+    #     null=True,
+    # )
 
     # サンプル項目2 メモ
     sample_2 = models.TextField(
