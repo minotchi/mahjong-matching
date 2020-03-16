d30ab08a4736e2c251b9783c943c2e23a8fce1a6
SHINTARO MINOWA <iyc4ouiw@gmail.com>
2020-03-16 18:41:44 +0900

    押し引きSQL

---
+++ oshihiki.sql
@@ -0,0 +1,360 @@
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('500', '5', '8', '0', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('1000', '10', '8', '0', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('1600', '15', '8', '0', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('2300', '20', '8', '0', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('3300', '25', '8', '0', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('1800', '5', '8', '0', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('2700', '10', '8', '0', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('3500', '15', '8', '0', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('4300', '20', '8', '0', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('5100', '25', '8', '0', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('400', '5', '11', '0', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('1000', '10', '11', '0', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('1600', '15', '11', '0', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('2200', '20', '11', '0', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('2900', '25', '11', '0', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('1300', '5', '11', '0', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('2000', '10', '11', '0', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('2700', '15', '11', '0', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('3500', '20', '11', '0', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('4600', '25', '11', '0', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('300', '5', '14', '0', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('500', '10', '14', '0', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('1000', '15', '14', '0', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('2200', '20', '14', '0', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('3000', '25', '14', '0', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('500', '5', '14', '0', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('1000', '10', '14', '0', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('2300', '15', '14', '0', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('3600', '20', '14', '0', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('4500', '25', '14', '0', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('0', '5', '8', '0', '1', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('1100', '10', '8', '0', '1', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('1500', '15', '8', '0', '1', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('2500', '20', '8', '0', '1', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('2900', '25', '8', '0', '1', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('1700', '5', '8', '0', '1', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('2500', '10', '8', '0', '1', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('3600', '15', '8', '0', '1', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('4900', '20', '8', '0', '1', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('6100', '25', '8', '0', '1', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('0', '5', '11', '0', '1', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('0', '10', '11', '0', '1', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('0', '15', '11', '0', '1', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('0', '20', '11', '0', '1', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('1900', '25', '11', '0', '1', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('0', '5', '11', '0', '1', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('0', '10', '11', '0', '1', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('2500', '15', '11', '0', '1', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('3400', '20', '11', '0', '1', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('4700', '25', '11', '0', '1', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('0', '5', '14', '0', '1', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('0', '10', '14', '0', '1', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('0', '15', '14', '0', '1', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('0', '20', '14', '0', '1', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('2100', '25', '14', '0', '1', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('0', '5', '14', '0', '1', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('0', '10', '14', '0', '1', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('0', '15', '14', '0', '1', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('0', '20', '14', '0', '1', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('4500', '25', '14', '0', '1', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('1100', '5', '8', '1', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('1800', '10', '8', '1', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('2600', '15', '8', '1', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('3300', '20', '8', '1', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('4300', '25', '8', '1', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('3000', '5', '8', '1', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('4000', '10', '8', '1', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('5100', '15', '8', '1', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('6400', '20', '8', '1', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('7900', '25', '8', '1', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('900', '5', '11', '1', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('1500', '10', '11', '1', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('2300', '15', '11', '1', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('3300', '20', '11', '1', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('4600', '25', '11', '1', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('2300', '5', '11', '1', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('3400', '10', '11', '1', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('4700', '15', '11', '1', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('6000', '20', '11', '1', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('7500', '25', '11', '1', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('500', '5', '14', '1', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('700', '10', '14', '1', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('1800', '15', '14', '1', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('3300', '20', '14', '1', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('4400', '25', '14', '1', '0', '1');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('900', '5', '14', '1', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('2000', '10', '14', '1', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('3700', '15', '14', '1', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('5400', '20', '14', '1', '0', '0');
+INSERT INTO "app_oshihiki"
+    ("point", "hoju_rate", "junme", "oponent_is_parent", "you_are_parent", "is_ryokei")
+VALUES
+    ('7200', '25', '14', '1', '0', '0');
