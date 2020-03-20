6c67a26ef70c086f268fcda70b895695a726ef63
SHINTARO MINOWA <iyc4ouiw@gmail.com>
2020-03-21 04:08:22 +0900

    UI変更（ラベルのフォントサイズ変更）

--- app/models.py
+++ app/models.py
@@ -430,9 +430,8 @@ class Oshihiki(models.Model):
 
     def get_rule_option(self):
         rule_option = []
-        rule_option.append({'id': 2, 'title': '天鳳ルール（段位戦 七段）'})
-        rule_option.append(
-            {'id': 1, 'title': 'Mリーグルール（25,000点持ち 30,000点返し ウマ1-3）'})
+        rule_option.append({'id': 2, 'title': '天鳳ルール', 'detail': '段位戦 七段'})
+        rule_option.append({'id': 1, 'title': 'Mリーグルール', 'detail': '25,000点持ち 30,000点返し ウマ1-3'})
 
         return rule_option
 
